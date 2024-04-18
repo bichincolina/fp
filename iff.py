9e7f22df2ff8baf4ab803315fa71566d01403c2e
bichincolina <bichincolina1@gmail.com>
2024-04-17 20:10:50 -0400

    ejerciccio

---
+++ iff.py
@@ -0,0 +1,46 @@
+# tacos=1
+# pizza=2
+# humitas=3
+# cazuela=4
+# print("elija una opcion de comida ")
+# op=int(input())
+# if op==(1):
+#     print("usted prefere los tacos")
+# elif(op==(2)):
+#     print("usted prefiere la pizza")
+# elif(op==(3)):
+#     print("usted prefiere la humita")
+# elif(op==(4)):
+#     print("usted prefiere la cazuela ")
+# else:
+#     print("usted es regodion")
+
+####
+# ticket_valido=False
+# folio_valido=123
+# print("ingrese el numero de folio")
+# n_folio=int(input())
+
+# if n_folio==folio_valido:
+#     ticket_valido=True
+    
+# if ticket_valido:
+#     print("este ticket es valido")
+# else: 
+#     print("este ticket caduco")
+
+
+edad=18
+grupo="A"
+color="azul"
+
+if edad>18 and grupo=="A":
+    print("usted es mayor y pertenece al grupo A ")
+elif edad>18 and grupo=="B":
+    print("usted es menor y pertenece al grupo B ")
+else:
+    print("usted no es mayor y no pertenece a ningun grupo  ")
+
+
+if color=="azul" or color=="negro " or color=="rojo": 
+    print("usted tiene por lo menos un color ")
--- pag.py
+++ pag.py
@@ -1,6 +1,16 @@
-print("ingrese un numero")
-for i in range(3):
+# print("ingrese un numero")
+# for i in range(3):
 
-    print("ingrese un numero")
-    num1=int(input())
-    print((num1+i)/3)
+#     print("ingrese un numero")
+#     num1=int(input())
+#     print((num1+i)/3)
+
+
+lista_numeros=[1,2,3,4,5,6,7,8]
+
+for valor in lista_numeros:
+    if valor>3:
+        print("el numero",valor, "es mayor que 3")
+    #print("este valor es",valor)
+
+
