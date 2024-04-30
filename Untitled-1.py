6fb4f5847a9a853544903ed43bff2a4a4bb935da
bichincolina <bichincolina1@gmail.com>
2024-04-30 19:09:25 -0400

    confirm

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,62 @@
-# Solicitar al usuario que ingrese su nombre
-nombre = input("Por favor, ingresa tu nombre: ")
 
-# Imprimir un saludo personalizado
-print("Hola,", nombre, "! Bienvenido a Python.")
+def multiPorDos():
+
+    print("Ingrese un numero")
+    num=int(input())
+    print(num*2)
+
+def resta5():
+    print("Ingrese un numero")
+    num=int(input())
+    print(num-5)
+
+def validaFolio():
+    ticket_valido=False
+    folio_valido=123
+    print("Ingrese el numero de folio")
+    n_folio=int(input())
+
+    if n_folio==folio_valido:
+        ticket_valido=True
+
+def suma(num1, num2):
+    resul=num1+num2
+    return resul
+def resta(num1, num2):
+    resul=num1-num2
+    return resul
+def multi(num1, num2):
+    resul=num1*num2
+    return resul
+def divi(num1, num2):
+    resul=num1/num2
+    return resul 
+while True:
+    print("Seleccione una opcion")
+    print("1.- -Suma")
+    print("2.- -Resta")
+    print("3.- -Multiplicacion")
+    print("4.- -Division")
+    op=int(input())
+
+    match op:
+        case 1:
+            print("Ingrese 2 numeros")
+            num1=int(input())
+            num2=int(input())
+            print("Su resultado es ",suma(num1,num2))
+        case 2:
+            print("Ingrese 2 numeros")
+            num1=int(input())
+            num2=int(input())
+            print("Su resultado es ",resta(num1,num2))
+        case 3:
+            print("Ingrese 2 numeros")
+            num1=int(input())
+            num2=int(input())
+            print("Su resultado es ",multi(num1,num2))
+        case 4:
+            print("Ingrese 2 numeros")
+            num1=int(input())
+            num2=int(input())
+            print("Su resultado es ",divi(num1,num2))
---
+++ fly.py
@@ -0,0 +1,109 @@
+# num=7
+
+# if num>0 and num%2==0:
+#     print("El numero es positivo y par")
+# else:
+#     print("El numero es positivo y no es par")
+
+
+
+
+# number=7
+# edad=45
+
+# if number>edad:
+#     print("La variable  es verdadera.")
+# elif edad > 50:
+#     print("La suma es mayor que 50.")
+# else:
+#     print("Ninguna de las condiciones anteriores se cumple.")
+
+# alex=15
+# sam=77
+# ada=60
+
+# if alex<sam:
+#     if sam>ada:
+#         print("Sam es el mayor de todos ")
+#     else:
+#         print("Sam solo es mayor que alex" ) 
+
+
+# leche_sin_lactosa=True
+# caja_habilitada=True
+# lider="abierto"
+# sistema_de_pago=False
+
+
+
+# if lider=="abierto":
+#     print("Puede entar al super")
+#     if leche_sin_lactosa:
+#         print("Hay leche sin lactosa")
+#         if caja_habilitada:
+#             print("Hay cajas habilitadas")
+#             if sistema_de_pago:
+#                 print("Puede pagar con debito")
+#             else:
+#                 print("Puede pagar con efectivo")
+
+# num1=19
+# num2=18
+# num3=31
+
+# if num1>num2 and num1>num3:
+#         print("el numero mayor es", num1)
+# elif num2>num3:
+#         print("el numero mayor es", num2)
+# else:
+#         print("el numero mayor es", num3) 
+
+
+# tacos=1
+# pizza=2
+# humitas=3
+# cazuela=4
+# print("Elija una opcion de comida")
+# op=int(input())
+
+# if op==(1):
+#         print("Usted prefiere los Tacos")
+# elif(op==(2)):
+#         print("Usted prefiere la pizza")
+# elif(op==(3)):
+#         print("Usted prefiere las humitas")
+# elif(op==(4)):
+#         print("Usted prefiere la cazuela")
+# else:
+#         print("Usted es regodion")
+
+###ejemplo de variable boleana  ###
+# ticket_valido=False
+
+# folio_valido=123
+# print("Ingrese el numero de folio")
+# n_folio=int(input())
+
+# if n_folio==folio_valido:
+#         ticket_valido=True
+
+# if ticket_valido:
+#         print("este ticket és valido")
+# else:
+#         print("este ticket caducó")
+
+# edad=19
+# grupo="A"
+
+# if edad>=18 and  grupo=="A":
+#         print("Used es mayor y pertenece al grupo A")
+# elif edad>=18 and  grupo=="B":
+#         print("Used es mayor y pertenece al grupo B")
+# else:
+#  print("Used no es mayor y no pertenece a ningun grupo")
+
+
+# color="Negro"
+
+# if color=="Azul" or  color=="Negro" or color=="Rojo":
+#         print("Usted tiene por lo menos un color")
